f449bc22007580bf0a3f66088f68d106c7807ff2
vegamissyl <jeomen@outlook.com>
2021-12-12 10:04:04 +0100

    change

--- ML/model.py
+++ ML/model.py
@@ -8,6 +8,6 @@ class ModelSeq(Sequential):
     def __init__(self):
         super(ModelSeq, self).__init__()
         self.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 4))
-        self.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 4))
+        self.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
         self.add(Dense(units = 4, kernel_initializer = 'uniform', activation = 'linear'))
         self.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics = [tf.keras.metrics.MeanSquaredError()])
