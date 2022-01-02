509b111c500e1f9a8d534d6caaa17de139e64e52
Neil FARMER <43957137+Neilstid@users.noreply.github.com>
2022-01-02 20:57:48 +0100

    Update setup.sh
    Just moved the kill of redis to be error safe

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@ chmod +x ./redis/Unix/redis-6.2.1/src/mkreleasehdr.sh
 cd ./redis/Unix/redis-6.2.1/
 make MALLOC=libc
 cd ./../../../
-kill `pidof redis-server`
 
 read -p 'Python command to use (for exmaple: python3): ' pcommand
 $pcommand setup.py
 
 COUNTER_FILE="run.sh"
 echo $pcommand" main.py" > $COUNTER_FILE
+
+kill `pidof redis-server`
