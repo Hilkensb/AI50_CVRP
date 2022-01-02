8d52a5c9066d99a1d1823a14bbbaa350e6c3b05b
Neil Farmer <neil@farmeronline.net>
2022-01-02 20:31:01 +0100

    Add benchmark result and kill redis
    Adding the results on the benchmarks (on the excel) and kill redis server after compilation

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ chmod +x ./redis/Unix/redis-6.2.1/src/mkreleasehdr.sh
 cd ./redis/Unix/redis-6.2.1/
 make MALLOC=libc
 cd ./../../../
+kill `pidof redis-server`
 
 read -p 'Python command to use (for exmaple: python3): ' pcommand
 $pcommand setup.py
